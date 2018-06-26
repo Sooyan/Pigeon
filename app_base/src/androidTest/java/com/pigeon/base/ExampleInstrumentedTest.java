0e64a457961d3fed0dfcb9a9f56a5182b3cf7b8d
yanm <yanm@shishike.com>
2018-06-26 18:09:24 +0800

    create new module-app_base

--- app_base/src/androidTest/java/com/pigeon/base/ExampleInstrumentedTest.java
+++ app_base/src/androidTest/java/com/pigeon/base/ExampleInstrumentedTest.java
@@ -1,4 +1,4 @@
-package com.pigeon.log;
+package com.pigeon.base;
 
 import android.content.Context;
 import android.support.test.InstrumentationRegistry;
@@ -21,6 +21,6 @@ public class ExampleInstrumentedTest {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getTargetContext();
 
-        assertEquals("com.pigeon.log.test", appContext.getPackageName());
+        assertEquals("com.pigeon.base.test", appContext.getPackageName());
     }
 }
